70205e22b5cd197e6097181696c29f17b803e9b8
Aditya Sinha <sinhadityaforever@gmail.com>
2022-10-12 21:48:10 +0530

    Changed timeline file

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,106 +1,105 @@
-import React, { useState, useRef, useEffect } from "react";
-import { RoughNotation } from "react-rough-notation";
+import React, { useState, useRef, useEffect } from 'react';
+import { RoughNotation } from 'react-rough-notation';
 import {
-  CarouselButton,
-  CarouselButtonDot,
-  CarouselButtons,
-  CarouselContainer,
-  CarouselItem,
-  CarouselItemImg,
-  CarouselItemText,
-  CarouselItemTitle,
-  CarouselMobileScrollNode,
-} from "./TimeLineStyles";
+	CarouselButton,
+	CarouselButtonDot,
+	CarouselButtons,
+	CarouselContainer,
+	CarouselItem,
+	CarouselItemImg,
+	CarouselItemText,
+	CarouselItemTitle,
+	CarouselMobileScrollNode
+} from './TimeLineStyles';
 import {
-  Section,
-  SectionDivider,
-  SectionText,
-  SectionTitle,
-} from "../../styles/GlobalComponents";
-import { TimeLineData } from "../../constants/constants";
+	Section,
+	SectionDivider,
+	SectionText,
+	SectionTitle
+} from '../../styles/GlobalComponents';
+import { TimeLineData } from '../../constants/constants';
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
-  const [activeItem, setActiveItem] = useState(0);
-  const carouselRef = useRef();
+	const [activeItem, setActiveItem] = useState(0);
+	const carouselRef = useRef();
 
-  const scroll = (node, left) => {
-    return node.scrollTo({ left, behavior: "smooth" });
-  };
+	const scroll = (node, left) => {
+		return node.scrollTo({ left, behavior: 'smooth' });
+	};
 
-  const handleClick = (e, i) => {
-    e.preventDefault();
+	const handleClick = (e, i) => {
+		e.preventDefault();
 
-    if (carouselRef.current) {
-      const scrollLeft = Math.floor(
-        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
-      );
+		if (carouselRef.current) {
+			const scrollLeft = Math.floor(
+				carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+			);
 
-      scroll(carouselRef.current, scrollLeft);
-    }
-  };
+			scroll(carouselRef.current, scrollLeft);
+		}
+	};
 
-  const handleScroll = () => {
-    if (carouselRef.current) {
-      const index = Math.round(
-        (carouselRef.current.scrollLeft /
-          (carouselRef.current.scrollWidth * 0.7)) *
-          TimeLineData.length
-      );
+	const handleScroll = () => {
+		if (carouselRef.current) {
+			const index = Math.round(
+				(carouselRef.current.scrollLeft /
+					(carouselRef.current.scrollWidth * 0.7)) *
+					TimeLineData.length
+			);
 
-      setActiveItem(index);
-    }
-  };
+			setActiveItem(index);
+		}
+	};
 
-  // snap back to beginning of scroll when window is resized
-  // avoids a bug where content is covered up if coming from smaller screen
-  useEffect(() => {
-    const handleResize = () => {
-      scroll(carouselRef.current, 0);
-    };
+	// snap back to beginning of scroll when window is resized
+	// avoids a bug where content is covered up if coming from smaller screen
+	useEffect(() => {
+		const handleResize = () => {
+			scroll(carouselRef.current, 0);
+		};
 
-    window.addEventListener("resize", handleResize);
-  }, []);
+		window.addEventListener('resize', handleResize);
+	}, []);
 
-  return (
-    <Section id="about">
-      <SectionDivider />
-      <div style={{ marginTop: "2rem" }}></div>
-      <SectionTitle>About Me</SectionTitle>
-      <SectionText>
-        I’m a software developer specializing in building{" "}
-        <RoughNotation
-          multiline={true}
-          show={true}
-          color="pink"
-          type="highlight"
-        >
-          full stack
-        </RoughNotation>{" "}
-        web apps. Currently, I’m looking for an internship at a seed or pre-seed
-        stage startup as a Full-Stack developer. I tried to start{" "}
-        <RoughNotation
-          multiline={true}
-          type="highlight"
-          show="true"
-          color="#86EFAC"
-        >
-          my first app based
-        </RoughNotation>{" "}
-        startup around a year ago, and got it deployed within a month. Currently
-        I am looking to be around people who are{" "}
-        <RoughNotation
-          multiline={true}
-          color="pink"
-          show={true}
-          type="highlight"
-        >
-          building products
-        </RoughNotation>{" "}
-        that bring a difference
-      </SectionText>
-      {/* <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
+	return (
+		<Section id="about">
+			<SectionDivider />
+			<div style={{ marginTop: '2rem' }}></div>
+			<SectionTitle>About Me</SectionTitle>
+			<SectionText>
+				I’m a software developer specializing in building{' '}
+				<RoughNotation
+					multiline={true}
+					show={true}
+					color="pink"
+					type="highlight"
+				>
+					full stack
+				</RoughNotation>{' '}
+				web apps. I tried to start{' '}
+				<RoughNotation
+					multiline={true}
+					type="highlight"
+					show="true"
+					color="#86EFAC"
+				>
+					my first app based
+				</RoughNotation>{' '}
+				startup around a year ago, and got it deployed within a month. Currently
+				I am looking to be around people who are{' '}
+				<RoughNotation
+					multiline={true}
+					color="pink"
+					show={true}
+					type="highlight"
+				>
+					building products
+				</RoughNotation>{' '}
+				that bring a difference
+			</SectionText>
+			{/* <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
           {TimeLineData.map((item, index) => (
             <CarouselMobileScrollNode
@@ -169,8 +168,8 @@ const Timeline = () => {
           );
         })}
       </CarouselButtons> */}
-    </Section>
-  );
+		</Section>
+	);
 };
 
 export default Timeline;
